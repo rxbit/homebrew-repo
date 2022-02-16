131befc387eea05c3c060d9567698602bdb665d3
hazytint@gmail.com <hazytint@gmail.com>
2022-02-16 16:08:31 +0800

    feat: fix install

--- Formula/rogcat.rb
+++ Formula/rogcat.rb
@@ -3,10 +3,9 @@ class Rogcat < Formula
   homepage "https://github.com/flxo/rogcat"
   url "https://github.com/flxo/rogcat/releases/download/v0.4.3/rogcat-x86_64-apple-darwin"
   sha256 "9948f339791a54475727670860376a8347b6fe359e4e19f8a1fa250274ad4f03"
-  bottle :unneeded
 
   def install
-    bin.install "rogcat"
+    bin.install "rogcat-x86_64-apple-darwin" => "rogcat"
   end
 
   test do
